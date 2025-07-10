7125f29b1de3da61b7165f4a1aabf272d7a12b7c
meAltf <alataf.dce.cs1@gmail.com>
2025-07-11 00:13:39 +0530

    filterContiguousArrEqualO1

--- PrefixSum/SubProblems/PrintAllSubArray.java
+++ PrefixSum/SubProblems/PrintAllSubArray.java
@@ -15,8 +15,26 @@ public class PrintAllSubArray {
         }
     }
 
+    private static void filterContiguousArrEqualO1(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            int cnt0 = 0, cnt1 = 0;
+            for (int j = i; j < n; j++) {
+                if (arr[j] == 0) cnt0++;
+                else cnt1++;
+                if (cnt0 == cnt1) {
+                    for (int k = i; k <= j; k++) {
+                        System.out.print(arr[k] + " ");
+                    }
+                    System.out.println();
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 0, 1};
-        printAllSubArr(arr);
+        // printAllSubArr(arr);
+        filterContiguousArrEqualO1(arr);
     }
 }
