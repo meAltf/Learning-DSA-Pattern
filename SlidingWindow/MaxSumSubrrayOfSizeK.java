f0a26c23f2203c7d2639efe471b746b2aa2ad396
meAltf <alataf.dce.cs1@gmail.com>
2025-07-16 08:54:21 +0530

    max sum subArray size k optimized | sliding window

--- SlidingWindow/MaxSumSubrrayOfSizeK.java
+++ SlidingWindow/MaxSumSubrrayOfSizeK.java
@@ -14,11 +14,30 @@ public class MaxSumSubrrayOfSizeK {
         return max;
     }
 
+    private static int maxiSumSubArrSizeKOptimized(int[] nums, int k) {
+        int n = nums.length, max = Integer.MIN_VALUE;
+        int WindowSum = 0;
+
+        // First window from start to end
+        for (int i = 0; i < k; i++) {
+            WindowSum += nums[i];
+        }
+        max = WindowSum;
+
+        // slide the window
+        for (int i = k; i < n; i++) {
+            WindowSum = WindowSum - nums[i - k] + nums[i];
+            max = Math.max(WindowSum, max);
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
         int[] arr = {2, 1, 5, 1, 3, 2};
         int k = 3;
 
         int result = maximumSumSubArrOfSizeK(arr, k);
-        System.out.println("result is: " + result);
+        int result2 = maxiSumSubArrSizeKOptimized(arr, k);
+        System.out.println("results are: " + result + "|" + result2);
     }
 }
