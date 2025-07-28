6958167ff43cd1d629335a70bb8fdffbeebe2b8b
meAltf <alataf.dce.cs1@gmail.com>
2025-07-28 19:26:03 +0530

    Reverse LL optimized

---
+++ LLInPlaceReversal/ReverseLLOptimized.java
@@ -0,0 +1,17 @@
+package LLInPlaceReversal;
+
+public class ReverseLLOptimized {
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+}
