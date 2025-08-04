b1707a632f44ca2aee35a4df972dc2850866dcd4
meAltf <alataf.dce.cs1@gmail.com>
2025-08-04 08:39:20 +0530

    Kth Largest Element in an Array | Optimized using minHeap

---
+++ TopKElements/findKthLargestElementOptimized.java
@@ -0,0 +1,24 @@
+package TopKElements;
+
+import java.util.PriorityQueue;
+
+public class findKthLargestElementOptimized {
+
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            minHeap.add(num);
+
+            if (minHeap.size() > k) minHeap.poll();
+        }
+        return minHeap.peek();
+    }
+
+    public static void main(String[] args) {
+        int[] input = {73, 74, 75, 71, 69, 72, 76, 73};
+        int result = findKthLargest(input, 3);
+
+        System.out.println(result);
+    }
+}
