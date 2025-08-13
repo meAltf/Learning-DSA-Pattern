8af1b4b86eb09b975823e90de2150393554fed3a
meAltf <alataf.dce.cs1@gmail.com>
2025-08-13 08:59:54 +0530

    merge interval | optimzed

--- OverlappingIntervals/mergeIntervals.java
+++ OverlappingIntervals/mergeIntervals.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class mergeIntervalsBruteForce {
+public class mergeIntervals {
 
     private static int[][] merge(int[][] intervals) {
         List<int[]> resultList = new ArrayList<>();
