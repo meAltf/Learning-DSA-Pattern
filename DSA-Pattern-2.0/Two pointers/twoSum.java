b729b8693b54c13251537fbd0c151a22397c876b
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2026-05-25 08:11:00 +0530

    Optimize twoSum method with hashmap approach
    Refactored twoSum method to use a hashmap for improved time complexity.

--- DSA-Pattern-2.0/Two pointers/twoSum.java
+++ DSA-Pattern-2.0/Two pointers/twoSum.java
@@ -1,5 +1,6 @@
-// 167. Two Sum II - Input Array Is Sorted | Brute-force
+// 167. Two Sum II - Input Array Is Sorted | 
 
+// Brute-force, O(n^2)
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         for(int i=1; i<=numbers.length; i++){
@@ -9,6 +10,23 @@ class Solution {
                     return new int[]{i,j};
                 } 
             }
+        }
+        return new int[]{-1,-1};
+    }
+}
+
+// Better approach using hashmap, means O(n) extra complexity but time complexity reduced to  O(n)
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<numbers.length; i++){
+            int res = target-numbers[i];
+
+            if(map.containsKey(res)){
+                return new int[]{map.get(res)+1, i+1};
+            }
+            map.put(numbers[i], i);
         }
         return new int[]{-1,-1};
     }
