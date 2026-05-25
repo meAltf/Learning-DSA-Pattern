29b133eee9aeb6de7eceaeeca2536748d5f9a941
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2026-05-25 09:08:03 +0530

    Add optimal two-pointer solution for twoSum II
    Implemented an optimal two-pointer approach for the twoSum  II problem.

--- DSA-Pattern-2.0/Two pointers/twoSum.java
+++ DSA-Pattern-2.0/Two pointers/twoSum.java
@@ -31,3 +31,18 @@ class Solution {
         return new int[]{-1,-1};
     }
 }
+
+// OPtimal approach | Two pointer approach O(n)
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int start=0, end=numbers.length-1;
+        for(int i=0; i<numbers.length; i++){
+            int sum = numbers[start]+numbers[end];
+            if(sum == target) return new int[]{start+1, end+1};
+            if(sum > target){
+                end--;
+            } else start ++;
+        }
+        return new int[]{-1,-1};
+    }
+}
